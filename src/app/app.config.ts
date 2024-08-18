db90cc591bf39055884b504dea2a84f87633d13d
unknown <j.quabeck@gmx.net>
2024-08-18 15:02:26 +0200

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-b3eb1","appId":"1:180299554180:web:d2a6cbf2817483b2bbd1d1","storageBucket":"simple-crm-b3eb1.appspot.com","apiKey":"AIzaSyDGoGP4IKmiruKfwaPUWVPxR9dsCF7UQdI","authDomain":"simple-crm-b3eb1.firebaseapp.com","messagingSenderId":"180299554180"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+};
